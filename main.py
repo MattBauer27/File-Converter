3d27b899aa5be8afd4b8434b548bc4eec1507f80
Matthew Bauer <bauermatt18@gmail.com>
2023-05-16 01:25:20 -0700

    Styled the GUI and added GeoJSON/Shapefile conversions

--- main.py
+++ main.py
@@ -1,97 +1,158 @@
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import ttk
+from tkinter import ttk, filedialog
 from PIL import Image
 from pdf2image import convert_from_path
+import geopandas as gpd
+import os
+import zipfile
+import tempfile
+
 
 class ConverterApp:
     def __init__(self, root):
         self.root = root
-        self.root.title('File Converter')
-
-        self.file_path = None
-        self.file_type_1 = tk.StringVar()
-        self.file_type_2 = tk.StringVar()
-
-        # Create widgets
+        self.root.title("File Converter")
+        self.file_path = ''
+        self.export_path = ''
+        self.file_type_1 = tk.StringVar(self.root)
+        self.file_type_2 = tk.StringVar(self.root)
+        self.file_types = ['.pdf', '.png', '.jpg', '.geojson', '.shp']
+        self.file_type_mappings = {
+            '.pdf': ['.png', '.jpg'],
+            '.png': ['.jpg', '.pdf'],
+            '.jpg': ['.png', '.pdf'],
+            '.geojson': ['.shp'],
+            '.shp': ['.geojson']
+        }
+        self.style = ttk.Style()
+        self.style.configure('TButton', font=('Arial', 10),
+                             background='green', foreground='black')
+        self.style.map('TButton',
+                       foreground=[('pressed', 'red'), ('active', 'red')],
+                       background=[('pressed', 'green'), ('active', 'green')]
+                       )
+        self.file_type_1.set('.pdf')  # Set the initial file_type_1
         self.create_widgets()
+        # Update file_type_2 options based on the initial file_type_1
+        self.update_file_type_2_options()
 
     def create_widgets(self):
-        # OptionMenu for file type 1
-        ttk.Label(self.root, text="Select input file type:").grid(column=0, row=0)
-        file_types_1 = ['.png', '.jpg', '.pdf']
-        self.option_menu_1 = ttk.OptionMenu(self.root, self.file_type_1, file_types_1[0], *file_types_1)
-        self.option_menu_1.grid(column=1, row=0)
+        ttk.Label(self.root, text="Select input file type:").grid(
+            column=0, row=0)
+        self.file_type_1_menu = ttk.OptionMenu(
+            self.root, self.file_type_1, *self.file_types)
+        self.file_type_1_menu.grid(column=1, row=0)
 
-        # Import file button
-        self.import_button = ttk.Button(self.root, text="Import File", command=self.import_file)
+        self.import_button = ttk.Button(
+            self.root, text="Import file", command=self.import_file)
         self.import_button.grid(column=0, row=1, columnspan=2)
 
-        # OptionMenu for file type 2
-        ttk.Label(self.root, text="Select output file type:").grid(column=0, row=2)
-        file_types_2 = ['.png', '.jpg', '.pdf']
-        self.option_menu_2 = ttk.OptionMenu(self.root, self.file_type_2, file_types_2[0], *file_types_2)
-        self.option_menu_2.grid(column=1, row=2)
+        self.file_type_2_menu = ttk.OptionMenu(
+            self.root, self.file_type_2, *self.file_type_mappings[self.file_type_1.get()])
+        self.file_type_2_menu.grid(column=1, row=2)
 
-        # Export file button
-        self.export_button = ttk.Button(self.root, text="Export File", command=self.export_file)
+        self.export_button = ttk.Button(
+            self.root, text="Export file", command=self.export_file)
         self.export_button.grid(column=0, row=3, columnspan=2)
 
-        # Run button
-        self.run_button = ttk.Button(self.root, text="Run", command=self.run_conversion)
+        self.run_button = ttk.Button(
+            self.root, text="Run", command=self.run_conversion)
         self.run_button.grid(column=0, row=4, columnspan=2)
 
-        # Progress bar
         self.progress = ttk.Progressbar(self.root, length=200)
         self.progress.grid(column=0, row=5, columnspan=2)
 
-        # Completion label
         self.completion_label = ttk.Label(self.root, text="")
         self.completion_label.grid(column=0, row=6, columnspan=2)
+        self.file_type_1.trace("w", self.update_file_type_1_options)
+        self.file_type_1.trace("w", self.update_file_type_2_options)
+
+    def update_file_type_1_options(self, *args):
+        menu = self.file_type_1_menu["menu"]
+        menu.delete(0, "end")
+        for string in self.file_types:
+            menu.add_command(label=string,
+                             command=lambda value=string: self.file_type_1.set(value))
 
     def import_file(self):
-        file_types = [(f"{self.file_type_1.get()} files", f"*{self.file_type_1.get()}")]
+        file_types = [(f"{self.file_type_1.get()} files",
+                       f"*{self.file_type_1.get()}"), ("All files", "*.*")]
+        if self.file_type_1.get() == '.shp':
+            file_types.insert(0, (".zip files", "*.zip"))
         self.file_path = filedialog.askopenfilename(filetypes=file_types)
+        if self.file_path.lower().endswith('.zip'):
+            temp_dir = tempfile.mkdtemp()
+            with zipfile.ZipFile(self.file_path, 'r') as zipf:
+                zipf.extractall(temp_dir)
+            shp_file = next((f for f in os.listdir(temp_dir)
+                            if f.lower().endswith('.shp')), None)
+            if shp_file is None:
+                self.completion_label['text'] = "No .shp file found in zip archive"
+                return
+            self.file_path = os.path.join(temp_dir, shp_file)
+
+    def update_file_type_2_options(self, *args):
+        menu = self.file_type_2_menu["menu"]
+        menu.delete(0, "end")
+        for string in self.file_type_mappings.get(self.file_type_1.get(), []):
+            menu.add_command(label=string,
+                             command=lambda value=string: self.file_type_2.set(value))
+        if self.file_type_mappings.get(self.file_type_1.get()):
+            self.file_type_2.set(
+                self.file_type_mappings.get(self.file_type_1.get())[0])
 
     def export_file(self):
-        file_types = [(f"{self.file_type_2.get()} files", f"*{self.file_type_2.get()}")]
-        self.export_path = filedialog.asksaveasfilename(defaultextension=self.file_type_2.get(), filetypes=file_types)
+        self.export_path = filedialog.asksaveasfilename(
+            defaultextension=self.file_type_2.get())
 
     def run_conversion(self):
         self.progress['value'] = 0
         self.completion_label['text'] = ""
-        print(f"Converting '{self.file_path}' from '{self.file_type_1.get()}' to '{self.file_type_2.get()}' and saving to '{self.export_path}'")
-        self.root.after(100, self.convert_file)
+        try:
+            if self.file_type_1.get() in ['.png', '.jpg', '.pdf']:
+                self.convert_image()
+            elif self.file_type_1.get() in ['.geojson', '.shp']:
+                self.convert_geo_data()
+        except Exception as e:
+            self.completion_label['text'] = str(e)
+            print(f"Error: {e}")  # print the exception to the console
 
-    def convert_file(self):
-        if self.file_type_1.get() != self.file_type_2.get():
-            if self.file_type_1.get() == '.pdf':
-                images = convert_from_path(self.file_path)
-                total = len(images)
-                for i in range(total):
-                    if self.file_type_2.get() == '.png':
-                        images[i].save(self.export_path if total == 1 else f"{self.export_path}_{i}.png", 'PNG')
-                    elif self.file_type_2.get() == '.jpg':
-                        images[i].convert('RGB').save(self.export_path if total == 1 else f"{self.export_path}_{i}.jpg", 'JPEG')
-                    self.progress['value'] = (i+1) / total * 100
-                    self.root.update_idletasks()
-            else:
-                image = Image.open(self.file_path)
-                if self.file_type_2.get() == '.png':
-                    image.save(self.export_path, 'PNG')
-                elif self.file_type_2.get() == '.jpg':
-                    image.convert('RGB').save(self.export_path, 'JPEG')
-                elif self.file_type_2.get() == '.pdf':
-                    image.save(self.export_path, 'PDF', resolution=100.0)
-                self.progress['value'] = 100
-                self.root.update_idletasks()
+    def convert_image(self):
+        if self.file_type_1.get() == '.pdf':
+            images = convert_from_path(self.file_path)
+            image = images[0]
+        else:  # PNG or JPEG
+            image = Image.open(self.file_path)
+        if self.file_type_2.get() == '.png':
+            image.save(self.export_path, 'PNG')
+        elif self.file_type_2.get() == '.jpg':
+            image.convert('RGB').save(self.export_path, 'JPEG')
+        elif self.file_type_2.get() == '.pdf':
+            image.convert('RGB').save(
+                self.export_path, 'PDF', resolution=100.0)
+        self.progress['value'] = 100
         self.completion_label['text'] = "Complete"
 
-def main():
+    def convert_geo_data(self):
+        geo_data = gpd.read_file(self.file_path)
+        if self.file_type_2.get() == '.geojson':
+            geo_data.to_file(self.export_path, driver='GeoJSON')
+        elif self.file_type_2.get() == '.shp':
+            geo_data.to_file(self.export_path, driver='ESRI Shapefile')
+            # Zip the shapefile components
+            base_path = os.path.splitext(self.export_path)[0]
+            with zipfile.ZipFile(f"{base_path}.zip", 'w') as zipf:
+                for ext in ['.shp', '.shx', '.dbf', '.prj', '.cpg']:
+                    if os.path.exists(f"{base_path}{ext}"):
+                        zipf.write(f"{base_path}{ext}", arcname=os.path.basename(
+                            f"{base_path}{ext}"))
+                        os.remove(f"{base_path}{ext}")
+            self.export_path = f"{base_path}.zip"
+        self.progress['value'] = 100
+        self.completion_label['text'] = "Complete"
+
+
+if __name__ == "__main__":
     root = tk.Tk()
     app = ConverterApp(root)
     root.mainloop()
-
-if __name__ == "__main__":
-    main()
-
